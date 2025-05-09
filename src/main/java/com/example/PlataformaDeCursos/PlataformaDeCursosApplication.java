133a5299b3fc706e901d24d5a529109fd337a0d0
PaulinhoLaniusJunior <paulinholaniusjunior@gmail.com>
2025-05-09 18:34:16 -0300

    SpringBoot e Docker
    Projeto base com Docker e as dependencias.
    o Spring Web (para API REST).
    o Spring JDBC.
    o Spring Data JPA.
    o MySQL Driver.
    o H2 (escopo test).

---
+++ src/main/java/com/example/PlataformaDeCursos/PlataformaDeCursosApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlataformaDeCursos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlataformaDeCursosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlataformaDeCursosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/PlataformaDeCursos/PlataformaDeCursosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PlataformaDeCursos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlataformaDeCursosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
